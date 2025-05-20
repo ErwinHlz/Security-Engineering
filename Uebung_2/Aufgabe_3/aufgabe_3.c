6e1047418c2a7548d893d86a4742a847275f6539
Erwin Holzhauser <erwinholzhauser.eh@gmail.com>
2025-05-20 02:06:24 +0200

    added comments

--- Uebung_2/Aufgabe_3/aufgabe_3.c
+++ Uebung_2/Aufgabe_3/aufgabe_3.c
@@ -16,7 +16,7 @@ void printTime(time_t t) {
 
 // Bestimmt den Dateityp aus dem Modusfeld (fileStat.st_mode)
 const char* getFileType(struct stat fileStat) {
-    switch (fileStat.st_mode & S_IFMT ) {
+    switch (fileStat.st_mode & S_IFMT ) {   // S_IFMT beschränkt .st_mode auf die Dateitypen "File Mode Type"
         case S_IFREG: return "Reguläre Datei";       // Diese sind alle in der Datei /usr/include/bits/stat.h definiert
         case S_IFDIR: return "Verzeichnis";
         case S_IFLNK: return "Symbolischer Link";
@@ -30,20 +30,20 @@ const char* getFileType(struct stat fileStat) {
 
 int main(int argc, char* argv[]) {
 
-    if (argc <  2) {
-        printf("Benutzung: %s <Dateipfad> [Dateipfad2] [..] \n", argv[0]);
+    if (argc <  2) {  // 2, weil der dateinname selbst auch als argument zählt, sprich bei < 1 würde das argument immer false sein, und das programm macht nichts ohne Pfade
+        printf("Du musst mindestens einen Dateipfad angeben. -> Benutzung: %s <Dateipfad> [Dateipfad2] [..] \n", argv[0]);
         return 1;
     }
 
   for (int i = 1; i < argc; i++) {
     const char* filePath = argv[i];
-    struct stat fileStat;
+    struct stat fileStat;  // struct stat speichert die eigenschaften, kann man lesen mit st_mode, st_size,...
 
       printf("\n==== Datei: %s ====\n", filePath);
       printf("\n");
 
     // Versuche, Informationen zur Datei zu lesen
-    if (lstat(filePath, &fileStat) == -1) {
+    if (lstat(filePath, &fileStat) == -1) {  //hier werden die dateien in filestat gespeichert deswegen &für speicheradresse
         perror("Fehler beim Lesen der Datei");
         continue;
     } else {
